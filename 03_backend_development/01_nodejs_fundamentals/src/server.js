ee90359596599f6fda0904b7e6f4502416e11ab4
lucasnfarias <lucasnfarias01@gmail.com>
2024-09-15 11:34:48 -0300

    create database persistence

--- 03_backend_development/01_nodejs_fundamentals/src/server.js
+++ 03_backend_development/01_nodejs_fundamentals/src/server.js
@@ -1,7 +1,9 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
 
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -13,13 +15,18 @@ const server = http.createServer(async (req, res) => {
       case "POST":
         const { name, email } = req.body;
 
-        users.push({
-          id: 1,
+        const user = {
+          id: randomUUID(),
           name,
           email,
-        });
+        };
+
+        database.insert('users', user)
+
         return res.writeHead(201).end();
       case "GET":
+        const users = database.select('users')
+
         return res.end(JSON.stringify(users));
     }
   }
